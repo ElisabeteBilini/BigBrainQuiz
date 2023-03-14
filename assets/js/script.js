65c3f79543f8a75d9c9d0cd13347377565067dec
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-03-14 11:38:46 +0000

    Add initial structure for Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//questions and answers
+
+const questions= [
+    {
+        title: ' The Cold War was between the United States and what other world power?',
+        answers: ['The Soviet Union', 'Japan', 'China', 'Canada'],
+        correct: 'The Soviet Union'
+    }
+
+]
