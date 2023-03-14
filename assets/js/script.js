c706f137080af5808dd5e1ea4dd72330ec34ddc4
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-03-14 20:10:08 +0000

    Fix script structure JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,101 +12,78 @@ let collectName = document.querySelector('.user');
 
 
 //-----------------------------------------GAME STRUCTURE---------------------------------------------------
-//--------------------------------------Codi Academy TV reference-------------------------------------------
-
-
+//-------------------------------------Codi Academy TV reference-------------------------------------------
 
 //questions and answers 
-let questions= [
+let questions = [
     {
         title: 'The Cold War was between the United States and what other world power?',
-        answers: [
-            {text: 'The Soviet Union', correct: true },
-            {text: 'Japan', correct: false },
-            {text: 'China', correct: false },
-            {text: 'Canada', correct: false },
-        ]
+        answers: ['The Soviet Union', 'Japan', 'China', 'Canada'],
+        correct: 0
     },
     {
         title: 'What scandal forced US President Richard Nixon to resign in 1974?',
-        answers: [
-            {text: 'Iran-Contra Affair', correct: false },
-            {text: 'Lewinsky Scandal', correct: false },
-            {text: 'Watergate Scandal', correct: true },
-            {text: 'Whitewater Scandal', correct: false },
-        ]
+        answers: ['Iran-Contra Affair', 'Lewinsky Scandal', 'Watergate Scandal', 'Whitewater Scandal'],
+        correct: 2
     },
     {
         title: 'Who was the first woman prime minister of Britain?',
-        answers: [
-            {text: 'Florence Nightingale', correct: false },
-            {text: 'Margaret Thatcher', correct: true },
-            {text: 'Queen Elizabeth II', correct: false },
-            {text: 'Jane Austen', correct: false },
-        ]
+        answers: ['Florence Nightingale', 'Margaret Thatcher', 'Queen Elizabeth II', 'Jane Austen'],
+        correct: 1
     },
     {
         title: 'Along with Hiroshima, Japan, which Japanese city was hit by an atomic bomb in 1945?',
-        answers: [
-            {text: 'Tokyo', correct: false },
-            {text: 'Osaka', correct: false },
-            {text: 'Nagoya', correct: false },
-            {text: 'Nagasaki', correct: true },
-        ]
+        answers: ['Tokyo', 'Osaka', 'Nagoya', 'Nagasaki'],
+        correct: 3
     },
     {
         title: 'Who assassinated Abraham Lincoln?',
-        answers: [
-            {text: 'Lee Harvey Oswald', correct: false },
-            {text: 'James Earl Ray', correct: false },
-            {text: 'John Wilkes Booth', correct: true },
-            {text: 'Leon Czolgosz', correct: false },
-        ]
+        answers: ['Lee Harvey Oswald', 'James Earl Ray', 'John Wilkes Booth', 'Leon Czolgosz'],
+        correct: 2
     },
     {
         title: 'Adolf Hitler was the leader of which party?',
-        answers: [
-            {text: 'Communist Party', correct: false },
-            {text: 'Fascist Republican Party', correct: false },
-            {text: 'Republican Party', correct: false },
-            {text: 'Nazi Party', correct: true },
-        ]
+        answers: ['Communist Party', 'Fascist Republican Party', 'Republican Party', 'Nazi Party'],
+        correct: 3
     },
     {
         title: 'What charter was approved by King John of England in 1215?',
-        answers: [
-            {text: 'The Mayflower Compact', correct: false },
-            {text: 'Stamp Act', correct: false },
-            {text: 'The Quartering Act', correct: false },
-            {text: 'Magna Carta', correct: true },
-        ]
+        answers: ['The Mayflower Compact', 'Stamp Act', 'The Quartering Act', 'Magna Carta'],
+        correct: 3
     },
     {
         title: 'Which continent was ravaged by the Black Death in the 1300s?',
-        answers: [
-            {text: 'Europe', correct: true },
-            {text: 'North America', correct: false },
-            {text: 'South America', correct: false },
-            {text: 'Africa', correct: false },
-        ]
+        answers: ['Europe', 'North America', 'South America', 'Africa'],
+        correct: 0
     },
     {
         title: 'Who created the first successful printer?',
-        answers: [
-            {text: 'Nikola Tesla', correct: false },
-            {text: 'Johannes Gutenberg', correct: true },
-            {text: 'Henry Ford', correct: false },
-            {text: 'Benjamin Franklin', correct: false },
-        ]
+        answers: ['Nikola Tesla', 'Johannes Gutenberg', 'Henry Ford', 'Benjamin Franklin'],
+        correct: 1
     },
     {
         title: 'What was the name of the first computer?',
-        answers: [
-            {text: 'INIAC', correct: false },
-            {text: 'CAINE', correct: false },
-            {text: 'ENIAC', correct: true },
-            {text: 'CNIEC', correct: false },
-        ]
-    }]
+        answers: ['INIAC', 'CAINE', 'ENIAC', 'CNIEC'],
+        correct: 2
+    }
+]
 
+function showQuestion(quest){
 
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent = quest.title;
+
+    let answers = document.querySelectorAll('.answers');
+    answers.forEach(function(element, index){
+        element.textContent = quest.answers[index];
+        element.addEventListener('click', function (){
+            if(index == quest.correct){
+                console.log("Right")
+            } else {
+                console.log("wrong")
+            }
+        })
+    })
+}
+
+showQuestion(answers);
