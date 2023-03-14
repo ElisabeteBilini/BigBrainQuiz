bca8518e2318c56296978431b476f56c96a04b2c
ELISABETE L BILINI <elisabetebilini@gmail.com>
2023-03-14 13:47:11 +0000

    Fix style, Add id, Fix script username, Add structure Javascript questions and answers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,37 @@
+ // Collect username from index.html page
+
+let btnInitial = document.querySelector('.initial-btn');
+let collectName = document.querySelector('.user');
+
+    btnInitial.addEventListener('click', () => {
+    let collectName = document.querySelector('.userName').value; 
+    localStorage.setItem('userName', collectName); 
+    })
+
+    if(collectName !== null) collectName.textContent = localStorage.getItem('userName');
+
+
+
 //questions and answers
-
-const questions= [
+let questions= 
     {
-        title: ' The Cold War was between the United States and what other world power?',
-        answers: ['The Soviet Union', 'Japan', 'China', 'Canada'],
-        correct: 'The Soviet Union'
-    }
+        title: 'What was the name of the first computer?',
+        answers: ['INIAC', 'CAINE', 'ENIAC', 'CNIEC'],
+        correct: 2
+    };
 
-]
+
+// show the question
+function LoadQuestion(questions){
+    let titleDiv = document.getElementById('title');
+    titleDiv.textContent = questions.title;
+
+// show the answers
+    let answers =  document.querySelectorAll('.answers');
+    //loop
+    answers.forEach(function(element, index){             
+        element.textContent = questions.answers[index];
+    });
+}
+
+LoadQuestion(questions);
